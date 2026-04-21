8fdd8efe21d899ef38256ec19cf8b652556f1962
medhamohiley29 <medhamohiley@gmail.com>
2026-04-21 21:06:08 +0530

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,13 @@
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        int sum = 0; 
+        
+        for (int num : numbers) {
+            sum += num; 
+        }
+        
+        System.out.println("The sum of all elements in the array is: " + sum);
+    }
+}
